9ba81e8746ac2b41a52ec7976f02ab6cbfc830a7
David Buckhurst <david.buckhurst@bbc.co.uk>
2014-05-20 22:59:57 +0100

    General improvement tweaks

---
+++ app/controllers/keys_controller.rb
@@ -0,0 +1,5 @@
+class KeysController < ApplicationController
+  def show
+    @key = "43krjhkgjh3kjg34"
+  end
+end
--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,12 +1,14 @@
 class WorldsController < ApplicationController
   def index
-    @worlds = World.last(20)
+    @worlds = World.last(20).reverse
   end
 
   def show
     @world = World.find(params[:id].to_i)
     @runs = @world.runs
-    @aggregate_results = AggregateResult.find( :world_id => @world.id )
+    @aggregate_results = AggregateResult.find( :world_id => @world.id ).sort { |a,b|
+      a.target + a.test_definition.name <=> b.target + b.test_definition.name
+    }
     @comparison_worlds = @world.similar
   end
 
@@ -14,7 +16,9 @@ class WorldsController < ApplicationController
     @reference = World.find(params[:reference].to_i)
     @primary = World.find(params[:primary].to_i)
 
-    @comparisons = AggregateResultComparison.find( params[:primary].to_i, params[:reference].to_i )
+    @comparisons = AggregateResultComparison.find( params[:primary].to_i, params[:reference].to_i ).sort { |a,b|
+      a.target + a.test_definition.name <=> b.target + b.test_definition.name
+    }
   end
   
   def comparison_element
