343dd4ea19928f2b8337ddc3876d63933cb89102
David Buckhurst <david.buckhurst@bbc.co.uk>
2014-08-08 22:18:38 +0100

    First pass user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :authenticate
+  def authenticate
+    @username = request.headers['bbc.name']
+    @email_address = request.headers['bbc.email_address']
+  end
 end
