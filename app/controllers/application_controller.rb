612265ef0284b8b3a772d21fbd00a808dbbab4e6
David Buckhurst <david.buckhurst@bbc.co.uk>
2014-09-03 22:28:28 +0100

    Count optimizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
   
-  before_filter :authenticate
-  def authenticate
-    @username = request.headers['bbc.name']
-    @email_address = request.headers['bbc.email_address']
+  private
+
+  def current_user
+    if request.headers['bbc.email_address']
+      @current_user ||= User.find_or_create(
+        :name => request.headers['bbc.name'], 
+        :email => request.headers['bbc.email_address'] )
+    end
   end
 end
