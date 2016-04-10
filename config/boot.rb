63e08982963820338b791d218b6932de3ecc52d4
David Buckhurst <david.buckhurst@bbc.co.uk>
2016-04-10 20:48:51 +0100

    Run by default on 3002

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Override rails defaults
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3002)
+    end
+  end
+end
