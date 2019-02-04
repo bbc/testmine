264edac962359a9f29c9f428308021ce8ebc2091
Qambar Raza <qambar.raza@bbc.co.uk>
2019-02-04 22:38:37 +0000

    Fix `Order By` clause is not in SELECT list
    I couldn't open the page http://192.168.99.100:3002/tests/28383?target=tv-platform-integration-test
    and was getting the SQL statement ORDER BY error.
    
    Found the following solution which worked:
    
    See https://github.com/rails/rails/issues/25924

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20160216102230) do
 
-  create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "test_definition_id"
     t.integer  "run_id"
     t.integer  "parent_id"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160216102230) do
     t.index ["test_definition_id"], name: "index_results_on_test_definition_id", using: :btree
   end
 
-  create_table "runs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "runs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "world_id"
     t.string   "owner"
     t.integer  "hive_job_id"
@@ -39,12 +39,10 @@ ActiveRecord::Schema.define(version: 20160216102230) do
     t.datetime "finished_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["hive_job_id"], name: "index_runs_on_hive_job_id", using: :btree
-    t.index ["target"], name: "index_runs_on_target", using: :btree
     t.index ["world_id"], name: "index_runs_on_world_id", using: :btree
   end
 
-  create_table "suites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "suites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "project"
     t.string   "name"
     t.string   "runner"
@@ -57,25 +55,25 @@ ActiveRecord::Schema.define(version: 20160216102230) do
     t.index ["project", "name"], name: "index_suites_on_project_and_name", using: :btree
   end
 
-  create_table "taggings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "taggings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "tag_id"
-    t.string   "taggable_type"
     t.integer  "taggable_id"
-    t.string   "tagger_type"
+    t.string   "taggable_type"
     t.integer  "tagger_id"
+    t.string   "tagger_type"
     t.string   "context",       limit: 128
     t.datetime "created_at"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
     t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
   end
 
-  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
 
-  create_table "test_definitions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "test_definitions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "name"
     t.string   "node_type"
     t.text     "description", limit: 65535
@@ -86,11 +84,10 @@ ActiveRecord::Schema.define(version: 20160216102230) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["name", "suite_id", "file", "parent_id"], name: "lookup", using: :btree
-    t.index ["parent_id"], name: "index_test_definitions_on_parent_id", using: :btree
     t.index ["suite_id"], name: "index_test_definitions_on_suite_id", using: :btree
   end
 
-  create_table "worlds", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "worlds", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "type"
     t.string   "name"
     t.text     "description", limit: 65535
@@ -101,7 +98,6 @@ ActiveRecord::Schema.define(version: 20160216102230) do
     t.datetime "updated_at"
     t.index ["project", "component", "version"], name: "index_worlds_on_project_and_component_and_version", using: :btree
     t.index ["project", "component"], name: "index_worlds_on_project_and_component", using: :btree
-    t.index ["project"], name: "index_worlds_on_project", using: :btree
   end
 
 end
